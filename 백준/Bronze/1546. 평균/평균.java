22e9da8b8d1805dcc704e94ef95fce612538a9ff
YOON <120346721+0702Yoon@users.noreply.github.com>
2024-05-13 22:58:05 +0900

    [Bronze I] Title: 평균, Time: 240 ms, Memory: 17752 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,15 +1,17 @@
 import java.util.Scanner;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = Integer.parseInt(sc.nextLine());
-		String[] line = sc.nextLine().split(" ");
-		IntStream st = Stream.of(line).mapToInt(Integer::parseInt);
-		int max = st.max().getAsInt();
-		System.out.println(
-				Stream.of(line).mapToDouble(Double::parseDouble).map(num -> num / max * 100).average().getAsDouble());
+		String[] number = sc.nextLine().split(" ");
+		int max = Integer.MIN_VALUE;
+		double sum = 0;
+		for (int i = 0; i < n; i++) {
+			if (max < Integer.parseInt(number[i]))
+				max = Integer.parseInt(number[i]);
+			sum += Integer.parseInt(number[i]);
+		}
+		System.out.println(sum / max * 100 / n);
 	}
 }
