c3a73a2c0ca8941d6fcd5ae36d0135e5bdad2b7f
한상윤(Han Sangyoon) <120346721+0702Yoon@users.noreply.github.com>
2025-04-21 18:34:45 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -0,0 +1,9 @@
+n, m = map(int,input().split(' '))
+baskets = list(range(1, n+1))
+
+for _ in range(m):
+    i, j = map(int,input().split(' '))
+    baskets[i-1: j] = baskets[i-1:j][::-1]
+
+for i in baskets:
+    print(i,end=' ')
