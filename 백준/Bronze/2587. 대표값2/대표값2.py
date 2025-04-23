15a58cfc1ec6ebe35cb231c43270c1d8f9a9ea5c
한상윤(Han Sangyoon) <120346721+0702Yoon@users.noreply.github.com>
2025-04-23 13:29:21 +0900

    [Bronze II] Title: 대표값2, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2587. 대표값2/대표값2.py
+++ 백준/Bronze/2587. 대표값2/대표값2.py
@@ -1,15 +1,10 @@
-def middle(a,b,c,d,e):
-    array = [a,b,c,d,e]
-    array.sort()
-    return array[2]
+a = int(input())
+b= int(input())
+c= int(input())
+d= int(input())
+e = int(input())
 
-if __name__ == "__main__":
-    a = int (input())
-    b =  int (input())
-    c =  int (input())
-    d =  int (input())
-    e =  int (input())
-    sum_result = a + b+c+ d +e
-    print(sum_result//5)
-    print(middle(a,b,c,d,e))
-    
+list = [a,b,c,d,e]
+list.sort()
+print((a+b+c+d+e)//len(list))
+print(list[len(list)//2])
